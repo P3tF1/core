c5bcb7fd4fb64202ca0f611d946491e96ca70eab
demigod <divyanshjain.2206@gmail.com>
2025-02-02 07:54:59 +0530

    fix build errors

--- app/games/rock-breaker/page.tsx
+++ app/games/rock-breaker/page.tsx
@@ -7,7 +7,6 @@ import { generateRocks, type RockType } from "./utils/rockUtils";
 import BackgroundScene from "./components/BackgroundScene";
 import StartTimer from "./components/StartTimer";
 import GameEndScene from "./components/GameEndScene";
-import Navbar from "@/components/navbar";
 import {Howler, Howl} from "howler"
 import { Navbar } from "@/components/Navbar";
 const GAME_DURATION = 10;
